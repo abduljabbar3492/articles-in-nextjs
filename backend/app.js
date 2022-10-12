bd627fb8d2ce1ee5c24cc483668ed46ac4b1708e
Abdul Jabbar <abduljabbar3492@gmail.com>
2022-10-12 12:42:18 +0300

    file path changed in app.js

--- backend/app.js
+++ backend/app.js
@@ -33,9 +33,9 @@ mongoose.connect(MONGODB_URL, {
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 1000000 }));
 app.use(cors());
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, '../website/build')));
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+    res.sendFile(path.join(__dirname, '../website/build/server/pages/index.html'));
 });
 // import routes index file 
 app.use('/api', router);
